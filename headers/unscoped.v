796993c7e28afea896094f0c4db8f094d0b60fa2
Yiyun Liu <liuyiyun@seas.upenn.edu>
2023-05-07 10:41:17 -0400

    Make the headers compatible with coq 8.17

--- headers/unscoped.v
+++ headers/unscoped.v
@@ -194,7 +194,7 @@ Tactic Notation "fsimpl" "in" "*" :=
 Notation "s , sigma" := (scons s sigma) (at level 60, format "s ,  sigma", right associativity) : subst_scope.
 
 Notation "s '..'" := (scons s ids) (at level 1, format "s ..") : subst_scope.
-Instance idsRen : Var nat nat := id.
+#[export]Instance idsRen : Var nat nat := id.
 
 Notation "↑" := (shift).
 
